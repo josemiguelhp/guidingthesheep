1022f85cbdb82d0369566bd4b5f31a38a2db3004
josemiguel_hp <josemiguelgames@gmail.com>
2018-12-07 22:03:55 -0300

    first commit!
    Signed-off-by: josemiguel_hp <josemiguelgames@gmail.com>

---
+++ android/src/com/guidingthesheep/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.guidingthesheep;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.guidingthesheep.Core;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Core(), config);
+	}
+}
